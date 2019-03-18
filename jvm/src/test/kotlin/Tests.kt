377005f9da526961749978b774829a50a86c8e6a
TheCompSciNoob <kyroschow@gmail.com>
2019-03-18 16:01:42 -0700

    Re-added tests for jvm

---
+++ jvm/src/test/kotlin/Tests.kt
@@ -0,0 +1,19 @@
+@file:Suppress("RemoveExplicitTypeArguments")
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class Tests {
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun testWithSampleRefs(): Unit = runBlocking<Unit> {
+        val results: List<QueueInfo> = runSimulation(
+            refs = createSampleRefs(),
+            timeout = 30.s
+        ).awaitAll()
+        val averageTime = results.map { it.totalWaitTime }.average()
+        println("\nAverage wait time: $averageTime")
+    }
+}
