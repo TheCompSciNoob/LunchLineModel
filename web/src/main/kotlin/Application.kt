385f5db25106149b3672ef8745764f8929bba64b
TheCompSciNoob <kyroschow@gmail.com>
2019-03-23 01:19:46 -0700

    Simplified lifecycle of webapp

--- web/src/main/kotlin/Application.kt
+++ web/src/main/kotlin/Application.kt
@@ -5,31 +5,54 @@ import kotlinx.html.*
 import kotlinx.html.dom.append
 import kotlinx.html.js.div
 import kotlinx.html.js.onClickFunction
-import org.w3c.dom.Document
 import org.w3c.dom.HTMLDivElement
 import kotlin.browser.document
 
-@ExperimentalCoroutinesApi
-fun Document.start(
-    qc: QueueController
-): HTMLDivElement {
+object Application {
+    val qc = QueueController()
     val div: HTMLDivElement by lazy {
         document.getElementById("simulation output") as HTMLDivElement
     }
 
-    fun onSimulationStop() {
+    //HTML builder, load UI
+    fun start() {
+        document.body!!.append.div {
+            h1 {
+                +"Lunch Line Simulation with Kotlin Coroutines"
+                style = "font-family:Verdana"
+            }
+            div {
+                button {
+                    +"Run Simulation"
+                    onClickFunction = { onSimulationStart() }
+                }
+                button {
+                    +"Clear"
+                    onClickFunction = { onSimulationStop() }
+                }
+            }
+            div {
+                id = "simulation output"
+                style = "font-family:Courier New;overflow:auto;padding:10px;white-space:pre"
+            }
+        }
+    }
+
+    private fun onSimulationStop() {
         qc.clear()
         div.textContent = ""
     }
 
-    fun onSimulationStart() {
+    @ExperimentalCoroutinesApi
+    private fun onSimulationStart() {
         onSimulationStop()
         qc.launch {
             div.textContent = ""
             val results: MutableList<QueueInfo> = mutableListOf()
             val resultsChannel: ReceiveChannel<QueueInfo> = qc.runSimulation(
                 refs = qc.createSampleRefs(),
-                timeout = 20.s
+                timeout = 20.s,
+                logging = false
             )
             for (queueInfo in resultsChannel) {
                 div.textContent = """
@@ -48,25 +71,7 @@ fun Document.start(
         }
     }
 
-    //UI
-    return body!!.append.div {
-        h1 {
-            +"Lunch Line Simulation with Kotlin Coroutines"
-            style = "font-family:Verdana"
-        }
-        div {
-            button {
-                +"Run Simulation"
-                onClickFunction = { onSimulationStart() }
-            }
-            button {
-                +"Clear"
-                onClickFunction = { onSimulationStop() }
-            }
-        }
-        div {
-            id = "simulation output"
-            style = "font-family:Courier New;overflow:auto;padding:10px;white-space:pre"
-        }
+    fun finish() {
+        qc.clear()
     }
 }
--- web/src/main/kotlin/Main.kt
+++ web/src/main/kotlin/Main.kt
@@ -4,11 +4,10 @@ import kotlin.browser.document
 @ExperimentalCoroutinesApi
 fun main() {
     //app lifecycle listeners
-    val qc = QueueController()
     document.addEventListener("DOMContentLoaded", {
-        document.start(qc)
+        Application.start()
     })
     document.addEventListener("unload", {
-        qc.clear()
+        Application.finish()
     })
 }
